fdf9a832d23d7a3f1792104f07f07d9c7d7ac009
Dean Fox <deanfoxcd@gmail.com>
2024-10-18 21:37:02 -0400

    add fracty in import

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { fracty } from 'fractional';
 import View from './view.js';
 
 class RecipeView extends View {
